9a64d9df54bf759a2d8047cd5ce0d84693ee5d22
mubarak-codes <mubarakcodes@gmail.com>
2025-05-17 22:32:47 +0100

    navbar

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -8,12 +8,8 @@ const Hero = ()=>{
   return(
     <>
       <section className="my5">
-        {/*search*/}
-        <div className="mb5 flex gap-5">
-          <input type="text" placeholder="Search..." className="w100 h50px mb2 form rounded-3"/>
-          <button className="bi bi-search btn-blue br2 px8 h50px"></ button>
-        </div>
-          <div className="mb5 flex gap9">
+        {/*search*/} 
+        <div className="mb5 flex gap2">
           <input type="text" placeholder="Search..." className="w100 h50px mb2 form rounded-3"/>
           <button className="bi bi-search btn-blue br2 px8 h50px"></ button>
         </div>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,11 +7,20 @@ const Navbar = ()=>{
   return(
     <>
       <section>
-        <nav className="navbar fixed-top bg-snow">
-          <div className="px5 px7-sm px9-md">
+        <nav className="navbar navbar-expand fixed-top bg-snow">
+          <div className="px5 px7-sm px9-md flex items-center w100">
            {/*nav brand*/}
             <div className="navbar-brand">
-              <h1 className="ff-rubik fs9 text-blue75">ONE SHOP</h1>
+              <h1 className="ff-rubik fs8 text-blue75">ONE SHOP</h1>
+            </div>
+
+            {/*nav items*/}
+            <div className="navbar-nav ms-self flex gap3 fs6 ">
+              <i className="bi bi-person-circle nav-item hover-opacity75"></i>
+              <div className="relative">
+                <i class="bi bi-cart nav-item hover-opacity75"></i>
+                <span className="absolute fs1 -right10px px1 br-circle bg-blue text-white">2</span>
+              </div>
             </div>
           </div>
         </nav>
